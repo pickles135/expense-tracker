19253d73171eba6e0f02200fbdbe7917a1f9b177
tcho <teresacho135@gmail.com>
2021-09-22 14:28:48 -0700

    creates globalState + context that is being used in transactionList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Header } from './components/Header';
-import { Balance } from './components/Balance';
-import { IncomeExpenses } from './components/IncomeExpenses';
-import { TransactionList } from './components/TransactionList';
-import { AddTransaction } from './components/AddTransaction';
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
 
-import { GlobalProvider } from './context/GlobalState';
+import { GlobalProvider } from "./context/GlobalState";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,14 +1,14 @@
-import React, { createContext, useReducer } from 'react';
-import AppReducer from './AppReducer';
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
 
 //Inital State
 const initialState = {
-    transactions: [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-    ]
+  transactions: [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 2, text: "Salary", amount: 300 },
+    { id: 3, text: "Book", amount: -10 },
+    { id: 4, text: "Camera", amount: 150 },
+  ],
 };
 
 //Create Context
@@ -16,11 +16,15 @@ export const GlobalContext = createContext(initialState);
 
 //Provider Component
 export const GlobalProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState);
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    return (<GlobalContext.Provider value={{
-        transactions: state.transactions
-    }}>
-        { children }
-    </GlobalContext.Provider>)
-}
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
