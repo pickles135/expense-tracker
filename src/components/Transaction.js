cbb81b21b544954de5f220ebeabc042e047e3ef7
tcho <teresacho135@gmail.com>
2021-09-22 15:07:54 -0700

    transaction.amount in transaction component is now dynamic with styling and data

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export const Transaction = () => {
+export const Transaction = ({ transaction }) => {
+    const sign = transaction.amount < 0 ? '-' : '+';
+
     return (
-    <li className="minus">
-        {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+        {transaction.text} <span>{sign}{Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
     </li>
     )
 }
