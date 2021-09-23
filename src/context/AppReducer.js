1baea1ac55f045f14b71a26ac333eb9f3e0b49af
Teresa Cho <tcho@cca.edu>
2021-09-22 20:38:35 -0700

    addTransaction action imported inside of addTransaction component and successfully completes front-end

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -5,6 +5,11 @@
                 ...state,
                 transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
             }
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [action.payload, ...state.transactions]
+            }
         default: 
             return state;
     }
